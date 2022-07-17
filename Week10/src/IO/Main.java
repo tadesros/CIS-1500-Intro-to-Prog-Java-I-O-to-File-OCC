a2243d8e7d37ca2cc0359d15ae315232c8685426
tadesros <tomadesrosiers@gmail.com>
2022-07-17 16:35:44 -0400

    after presentation

--- Week10/src/IO/Main.java
+++ Week10/src/IO/Main.java
@@ -5,63 +5,125 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    //MAIN
     public static void main(String[] args) {
-
+        //Input Scanner
         Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("Do you want to 1-create/update a file or 2-read a file?");
-
+        //Prompt main option
+        System.out.println("Do you want to 1 - create/update a file or 2-read a file?");
+        //Get Choice
         String choice = keyboard.nextLine();
+        //Check Main Choice
+        if(choice.equalsIgnoreCase("1")) {
+            //Get filename
+            System.out.println("Enter the name of the class to save the list to: ");
+            String className = keyboard.nextLine();
+            //Add correct extension
+            File file = new File(className + ".txt");
 
-
-        if(choice.equalsIgnoreCase(
-                "1"))
-        {
-
-            ArrayList<String> names = new ArrayList<>();
-            System.out.println("Enter a student name or DONE when finished: ");
-
-            String name = keyboard.nextLine();
-            while (!name.equalsIgnoreCase("Done")){
-                names.add(name);
-                System.out.println("Enter a student name or DONE when finished: ");
-                name = keyboard.nextLine();
+            //File does not exist
+            if (!file.exists()) {
+                ArrayList<Student> students = new ArrayList<>();
+                //Add Names
+                addStudentsToList(keyboard, students);
+                //Write to a file
+                writeFile(className, students);
             }
+            //File Does Exist
+            else {
+                ArrayList<Student> students = new ArrayList<>();
+                readFileIntoList(file, students);
 
+                System.out.println("Names currently in the file: ");
+
+                for (Student student : students) {
+                    System.out.println(student.getName());
+                }
+
+                System.out.println("Do you want to add any? y/n");
+                String add = keyboard.nextLine();
+                if (add.equalsIgnoreCase("y")) {
+                    addStudentsToList(keyboard, students);
+                }
+                    //Write the file again
+                    writeFile(className, students);
+            }
+        }
+        else if(choice.equalsIgnoreCase("2")){
+            ArrayList<Student> students = new ArrayList<>();
+            //Get filename
             System.out.println("Enter the name of the class to save the list to: ");
             String className = keyboard.nextLine();
 
-            //Write to a file
-            try {
-                //Write a file out - open file - doesn't exist creates a new file
-                //Erases what was their before
-                PrintWriter fileOutput = new PrintWriter(new BufferedWriter(new FileWriter(className +".txt")));
-                //Write to the file
-                for(String nameList : names)
-                {
-                    fileOutput.println(nameList);
-                }
-                //Close the file
-                fileOutput.close();
-            } catch (IOException ex) {
-                //Print out the exception.
-                System.out.println(ex);
+            //Add correct extension
+            File file = new File(className + ".txt");
+
+            readFileIntoList(file,students);
+
+            //Print them out
+            for(Student student: students){
+                System.out.println(student.getStringForFileOutput());
             }
 
-       }
-        else {
 
         }
 
-
-
-
-
-
     } //End main function
 
-    public static void fileExample(){
+    private static void readFileIntoList(File file, ArrayList<Student> students) {
+        try {
+            //Read the file
+            BufferedReader inputFile = new BufferedReader(
+                    new FileReader(file));
+            //Read the file
+            String line = inputFile.readLine();
+            while (line != null) {
+                //If it is not empty skip writing
+                if (!line.isEmpty()) {
+                   String[] values =  line.split("\\|");
+                    students.add(new Student(values[0],Integer.parseInt(values[1])));
+                }
+                line = inputFile.readLine();
+            }
+            //Close the file
+            inputFile.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 
+    //METHODS
+    private static void addStudentsToList(Scanner keyboard, ArrayList<Student> students) {
+        System.out.println("Enter a student name or DONE to be all done: ");
+        String name = keyboard.nextLine();
+
+        System.out.println("Enter their Score ");
+        int score = Integer.parseInt(keyboard.nextLine());
+
+        while (!name.equalsIgnoreCase("Done")) {
+            students.add(new Student(name,score));
+            System.out.println("Enter a student name or DONE when finished: ");
+            name = keyboard.nextLine();
+            System.out.println("Enter their Score ");
+            score = Integer.parseInt(keyboard.nextLine());
+        }
+    }
+    private static void writeFile(String className, ArrayList<Student> students) {
+        try {
+            //Write a file out - open file - doesn't exist creates a new file
+            PrintWriter fileOutput = new PrintWriter(new BufferedWriter(new FileWriter(className + ".txt")));
+            //Write to the file
+            for (Student student: students) {
+                fileOutput.println(student.getStringForFileOutput());
+            }
+            //Close the file
+            fileOutput.close();
+        } catch (IOException ex) {
+            //Print out the exception.
+            System.out.println(ex);
+        }
+    }
+    public static void fileExample(){
         //Write to a file
         try {
             //Write a file out - open file - doesn't exist creates a new file
@@ -76,7 +138,6 @@ public class Main {
             //Print out the exception.
             System.out.println(ex);
         }
-
 
         //Read from a file
         File file = new File("test.txt");
@@ -101,8 +162,6 @@ public class Main {
         else{
             System.out.println("Doesn't exist");
         }
-
     }
 
-
-}//End Class
+}//End Main Class
